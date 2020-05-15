57be3574f408cd887ecbd6c9cc5054f4bf1b7388
ihteshamm123 <39794328+ihteshamm123@users.noreply.github.com>
2020-05-15 05:00:53 -0700

    unity file

--- car_game_unity/Assets/Scripts/PlayerController.cs
+++ car_game_unity/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,25 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-    public float speed = 20.0f;
+    //private veriables
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    
     // Update is called once per frame
     void Update()
     {
+        // where we get player input.
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // Move the vehicle forward
         //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //We move the vehicle forward.
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //We rotate the vehicle left to right.
+        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
