5febcc6fc9921b426576d37428ba5b9678464e76
ihteshamm123 <39794328+ihteshamm123@users.noreply.github.com>
2020-05-15 04:41:08 -0700

    update

---
+++ car_game_unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed = 20.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward
+        //transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
